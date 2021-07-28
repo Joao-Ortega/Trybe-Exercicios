2430968aaab846410f9c43827ebada45a7563326
Joao-Ortega <joao_ortega17@hotmail.com>
2021-07-28 15:58:17 -0300

    Primeiro exercício de if/else

---
+++ fundamentos/bloco-4-introducao-javascript/dia-1-introducao-javascript-logica-de-programacao/scripts.js
@@ -0,0 +1,10 @@
+let nota = 72.56;
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!")
+}
+else if (nota < 80 && nota >= 60) {
+    console.log("Você está na nossa lista de espera");
+}
+else {
+    console.log("Você foi reprovada(o)");
+}
