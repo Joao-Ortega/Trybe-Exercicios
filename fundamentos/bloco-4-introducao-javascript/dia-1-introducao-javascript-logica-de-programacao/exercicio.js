911eb9376e92d8f3d58ef2bb7a887ba9bbffa142
Joao-Ortega <joao_ortega17@hotmail.com>
2021-07-28 18:31:14 -0300

    Exercício 2 de JS

--- fundamentos/bloco-4-introducao-javascript/dia-1-introducao-javascript-logica-de-programacao/exercicio.js
+++ fundamentos/bloco-4-introducao-javascript/dia-1-introducao-javascript-logica-de-programacao/exercicio.js
@@ -1,5 +1,5 @@
 //Exercício número 1.
-let a = 8;
+/*let a = 8;
 let b = 7;
 let resultadoSoma = a+b;
 console.log(resultadoSoma);
@@ -14,5 +14,17 @@ let resultadoDivisao = a/b;
 console.log(resultadoDivisao);
 
 let resultadoModulo = a%b;
-console.log(resultadoModulo);
+console.log(resultadoModulo);*/
+
+//Exercício número 2.
+
+let number1 = 51;
+let number2 = 394;
+if (number1 > number2) {
+    console.log(number1);
+}
+else {
+    console.log(number2);
+}
+
 
