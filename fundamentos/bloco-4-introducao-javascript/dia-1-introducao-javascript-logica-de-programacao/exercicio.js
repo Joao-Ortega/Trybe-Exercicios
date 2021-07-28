596b78e3edbfdc7bb68bf8dc1cacc3e0392a350a
Joao-Ortega <joao_ortega17@hotmail.com>
2021-07-28 18:32:29 -0300

    Exercício 3 de JS

--- fundamentos/bloco-4-introducao-javascript/dia-1-introducao-javascript-logica-de-programacao/exercicio.js
+++ fundamentos/bloco-4-introducao-javascript/dia-1-introducao-javascript-logica-de-programacao/exercicio.js
@@ -18,13 +18,27 @@ console.log(resultadoModulo);*/
 
 //Exercício número 2.
 
-let number1 = 51;
+/*let number1 = 51;
 let number2 = 394;
 if (number1 > number2) {
     console.log(number1);
 }
 else {
     console.log(number2);
-}
+}*/
 
+//Exercício número 3.
+
+let number1 = 276;
+let number2 = 25;
+let number3 = 971;
+if(number1 > number2 && number1 > number3) {
+    console.log(number1);
+}
+else if(number2 > number1 && number2 > number3) {
+    console.log(number2);
+}
+else {
+    console.log(number3);
+}
 
