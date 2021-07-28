79fdbd57b2df696c7ba7dfd03076f1bf18c55587
Joao-Ortega <joao_ortega17@hotmail.com>
2021-07-28 18:30:01 -0300

    Exercício 1 JS

---
+++ fundamentos/bloco-4-introducao-javascript/dia-1-introducao-javascript-logica-de-programacao/exercicio.js
@@ -0,0 +1,18 @@
+//Exercício número 1.
+let a = 8;
+let b = 7;
+let resultadoSoma = a+b;
+console.log(resultadoSoma);
+
+let resultadoSubtracao = a-b;
+console.log(resultadoSubtracao);
+
+let resultadoMultiplicacao = a*b;
+console.log(resultadoMultiplicacao);
+
+let resultadoDivisao = a/b;
+console.log(resultadoDivisao);
+
+let resultadoModulo = a%b;
+console.log(resultadoModulo);
+
