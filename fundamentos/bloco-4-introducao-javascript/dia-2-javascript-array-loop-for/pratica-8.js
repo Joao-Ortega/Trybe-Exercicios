c1dec57af8d134aa11121b4b9dde8a1a3e095ef7
Joao-Ortega <joao_ortega17@hotmail.com>
2021-07-29 20:57:30 -0300

    Divindo todos os elementos por 2

--- fundamentos/bloco-4-introducao-javascript/dia-2-javascript-array-loop-for/pratica-8.js
+++ fundamentos/bloco-4-introducao-javascript/dia-2-javascript-array-loop-for/pratica-8.js
@@ -2,4 +2,3 @@ let array = [];
 for(let count = 1; count <= 25; count += 1) {
     array.push(count);
 } console.log(array);
-
---
+++ fundamentos/bloco-4-introducao-javascript/dia-2-javascript-array-loop-for/pratica-9.js
@@ -0,0 +1,5 @@
+let divisaoPorDois;
+for(let count = 1; count <= 25; count += 1) {
+    divisaoPorDois = count/2;
+    console.log(divisaoPorDois);
+} 
