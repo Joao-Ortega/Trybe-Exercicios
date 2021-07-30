5044be71c4ed5b1cac6bd09ace5895fa967ed53a
Joao-Ortega <joao_ortega17@hotmail.com>
2021-07-30 15:00:46 -0300

    Exercitando lógica de programação

---
+++ fundamentos/bloco-4-javascript-logica/dia-3-logica-de-programacao-e-algoritmo/exercitando-logica.js
@@ -0,0 +1,8 @@
+let fruits = [3, 4, 10, 1, 12];
+let resultado = 0;
+for(let index = 0; index < fruits.length; index += 1) {
+    resultado += fruits[index];
+} if(resultado >15)
+    console.log(resultado)
+  else 
+    console.log("valor menor que 16");
