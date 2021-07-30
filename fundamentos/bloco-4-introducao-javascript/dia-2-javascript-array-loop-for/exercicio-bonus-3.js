6e724a2e04cd7112d04d5e507e89cfe91a20c4a7
Joao-Ortega <joao_ortega17@hotmail.com>
2021-07-30 11:54:04 -0300

    Multiplicando todos os elementos do array pelo seguinte e o último por 2

---
+++ fundamentos/bloco-4-introducao-javascript/dia-2-javascript-array-loop-for/exercicio-bonus-3.js
@@ -0,0 +1,19 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let arrayNumerosMultiplicados = [];
+
+for(let index = 0; index < numbers.length; index += 1) {
+    for(var secondIndex = 1; secondIndex < numbers.length; secondIndex += 1) {
+        arrayNumerosMultiplicados.push(numbers[index] * numbers[secondIndex]);
+        index += 1;
+    }
+    if(secondIndex >= numbers.length) {
+        arrayNumerosMultiplicados.push(numbers[index] * 2);
+    }
+} console.log(arrayNumerosMultiplicados);
+
+
+//Percorrer o array numbers pegando a primeira posição;
+//Percorrer o array numbers pegando a segunda posição;
+//Multiplicar a primeira posição com a segunda e armazenar com .push no novo array;
+//Fazer com que volte para o primeiro for ou outra opção;
+// caso seja a última posição e a segunda posição dê false, multiplique o valor do primeiro for por 2 e armazene também no novo array;
