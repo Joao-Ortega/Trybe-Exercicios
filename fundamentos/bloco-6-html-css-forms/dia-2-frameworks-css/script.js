45996caff8bcce2a3ed22a172b459b2c7695fb2b
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-20 11:39:01 -0300

    Arrumando um pouco mais

--- fundamentos/bloco-6-html-css-forms/dia-2-frameworks-css/script.js
+++ fundamentos/bloco-6-html-css-forms/dia-2-frameworks-css/script.js
@@ -1,7 +1,5 @@
 const selectDropDown = document.getElementById('allStates');
 const submitButton = document.querySelector('#submitButton');
-const div = document.createElement('div');
-document.body.appendChild(div);
 const allInputs = document.querySelectorAll('.inputs');
 function createAllStateList() {
   const arrayState = [ 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MS', 'MT', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO',]; 
@@ -15,7 +13,9 @@ function createAllStateList() {
 function stopButton(e) {
   const event = e
   event.preventDefault();
-  event.preventDefault();
+  const div = document.createElement('div');
+document.body.appendChild(div);
+div.className = 'card-panel';
   for (let index of allInputs) {
     let newP = document.createElement('p');
     newP.innerText = index.getAttribute('name') +  index.value;
