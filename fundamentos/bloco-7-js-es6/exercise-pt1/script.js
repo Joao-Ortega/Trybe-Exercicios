28e62b2d91c9b5cc32c1352d40608d345eb6f747
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-28 14:29:43 -0300

    Exercícios parte 1

--- fundamentos/bloco-7-js-es6/exercise-pt1/script.js
+++ fundamentos/bloco-7-js-es6/exercise-pt1/script.js
@@ -11,7 +11,7 @@
 } */
 
 /* testingScope(true); */
-// Modificando código acima
+// Parte 1 ex 1 Modificando código acima
 const testingScope = escopo => {
   if (escopo === true) {
     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -23,3 +23,9 @@ const testingScope = escopo => {
   }
 }
 testingScope(true);
+// ex 2
+const oddsAndEvens = [ 4, 10,13, 3, 7, 2, 2, 12, 24];
+// Estou declarando uma constante que recebe uma função, que ordena o Array oddsAndEvens, e essa função de ordenamento recebe como parâmetros 2 argumentos (que seria dois elementos do array oddsAndEvens) e faz a subtração de um com outro; sendo resultado positivo o primeiro número é maior
+oddsAndEvens.sort((a, b) => a-b);
+ 
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
