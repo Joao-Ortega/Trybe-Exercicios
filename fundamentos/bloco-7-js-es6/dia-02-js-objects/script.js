94f9c3d8d7555acab88b679d80db1765b76d2a04
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-30 18:06:32 -0300

    Exercício 05

--- fundamentos/bloco-7-js-es6/dia-02-js-objects/script.js
+++ fundamentos/bloco-7-js-es6/dia-02-js-objects/script.js
@@ -124,3 +124,5 @@ const allKeys = obj => Object.keys(obj);
 const lengthOfObject = obj => Object.keys(obj).length;
 // número 04 Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 const allValues = obj => Object.values(obj);
+// número 05 Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 .
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
