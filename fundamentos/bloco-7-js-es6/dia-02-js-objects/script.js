2785a24b153611cb8bdbae77e8a546431250b842
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-30 12:35:05 -0300

    Parte II Object.keys

--- fundamentos/bloco-7-js-es6/dia-02-js-objects/script.js
+++ fundamentos/bloco-7-js-es6/dia-02-js-objects/script.js
@@ -9,3 +9,26 @@ const addToObject = (obj, newKey, valueKey) => {
 }
 addToObject(pessoa1, 'nacionalidade', 'Brasileiro');
 console.log(pessoa1);
+// Tente criar uma função que exiba as habilidades do objeto student . Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade". 
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const skills = student => {
+  const arrayOfObjects = Object.keys(student);
+  for (index in arrayOfObjects) {
+    console.log(`${arrayOfObjects[index]}, Nível: ${student[arrayOfObjects[index]]}`);
+  }
+}
+skills(student1);
