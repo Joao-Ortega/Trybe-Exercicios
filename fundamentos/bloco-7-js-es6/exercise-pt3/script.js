67625ff1708e5381aba7cfb5c6ec1abf7bf46464
Joao-Ortega <joao_ortega17@hotmail.com>
2021-08-30 10:21:17 -0300

    Finalizando exercícios 7.1

--- fundamentos/bloco-7-js-es6/exercise-pt3/script.js
+++ fundamentos/bloco-7-js-es6/exercise-pt3/script.js
@@ -12,3 +12,17 @@ const counter = () => {
 }
 
 button.addEventListener('click', counter);
+
+// exercise 3
+let string = "Tryber x aqui!";
+const replaceX = text => {
+  let newString = string.replace('x', `${text}`);
+  return newString;
+}; console.log(replaceX('Bebeto'));
+// Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+let mainSkills = ['HTML', 'JS', 'CSS', 'GIT', 'SoftSkills'];
+// Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
+const greeting = returnOfAFunction => {
+  let hello = `${returnOfAFunction} Minhas cinco principais habilidades são:${mainSkills.sort()}`;
+  return hello;
+}; console.log(greeting(replaceX('João')));
