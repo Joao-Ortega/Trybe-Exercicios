d5fe0f40e9698481a459272f52ac1a1759ae328e
Joao-Ortega <joao_ortega17@hotmail.com>
2021-09-02 19:22:06 -0300

    Exercício 03

--- fundamentos/bloco-8-higher-order-functions-js/dia-01-introducao-hofs/script.js
+++ fundamentos/bloco-8-higher-order-functions-js/dia-01-introducao-hofs/script.js
@@ -24,11 +24,11 @@ const newEmployees = (func) => {
     id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
-}; 
+};
 // 02
 const playerNumber = 2;
 const winCheck = (yourNumber, n) => {
-  if (yourNumber === n){
+  if (yourNumber === n) {
     return true;
   } else {
     return false;
@@ -36,9 +36,33 @@ const winCheck = (yourNumber, n) => {
 }
 const hof = (playerNumber, func) => {
   const luckyNumber = Math.floor(Math.random() * 5) + 1;
-  if (func(playerNumber, luckyNumber)){
+  if (func(playerNumber, luckyNumber)) {
     return 'Parabéns você ganhou!';
   } else {
     return 'Tente novamente!';
   }
 };
+// 03
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+let points = 0;
+const testIfAproval = (arr1, arr2) => {
+  for (let index = 0; index < arr1.length; index += 1) {
+    for (let index2 = 0; index2 < arr2.length; index2 += 1) {
+      if (arr1[index] === arr2[index2]) {
+        points += 1;
+      } else if (arr1[index] !== arr2[index2]) {
+        points - 0.5;
+      } else if (arr2[index2] === 'N.A') {
+        points;
+      }
+    }
+  }
+  return points;
+}
+const higherOrder = (template, check, callback) => {
+  return `Gabarito: ${template}
+  Respostas: ${check}
+  Pontuação: ${callback(template, check)}`;
+};
+
