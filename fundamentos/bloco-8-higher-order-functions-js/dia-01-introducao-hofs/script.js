0904a023fa3a6c690d0d5e0c55a545dda7bbf306
Joao-Ortega <joao_ortega17@hotmail.com>
2021-09-06 11:26:06 -0300

    Bônus

--- fundamentos/bloco-8-higher-order-functions-js/dia-01-introducao-hofs/script.js
+++ fundamentos/bloco-8-higher-order-functions-js/dia-01-introducao-hofs/script.js
@@ -43,27 +43,95 @@ const hof = (playerNumber, func) => {
   }
 };
 // 03
-const RIGHT_ANSWERS =   ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
 const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
 let points = 0;
 const testIfAproval = (arr1, arr2) => {
   for (let index = 0; index < arr1.length; index += 1) {
-      if (arr1[index] === arr2[index]) {
-        points += 1;
-      } else if (arr2[index] === 'N.A') {
-        points = points;
-      } else if (arr1[index] !== arr2[index]) {
-        points -= 0.5;
-      }
+    if (arr1[index] === arr2[index]) {
+      points += 1;
+    } else if (arr2[index] === 'N.A') {
+      points = points;
+    } else if (arr1[index] !== arr2[index]) {
+      points -= 0.5;
     }
-    return points;
-  }; 
- 
+  }
+  return points;
+};
+
 const higherOrder = (template, check, callback) => {
   return `Gabarito: ${template}
   Respostas: ${check}
   Pontuação: ${callback(template, check)}`;
 };
 // bônus
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
 
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+// Dano Dragon
+const dragonDamage = () => {
+  const drgDamage = Math.floor(Math.random() * (50 - 15 + 1) + 15);
+  return drgDamage;
+};
+// Dano Warrior
+const warriorDamage = () => {
+  const warrDamage = Math.floor(Math.random() * (60 - 30 + 1) + 30);
+  return warrDamage;
+};
+// Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+const roundForMage = () => {
+  const mageShift = {
+    damage: Math.floor(Math.random() * (90 - 45 + 1) + 45),
+    mana: 15,
+  }
+  if (mage.mana < 15) {
+    mageShift.damage = 'You dont have enough mana!';
+    mageShift.mana = 0;
+    return mageShift;
+  } else {
+    return mageShift;
+  }
+};
+
+const gameActions = {
+  warriorShift: (warriorDamage) => {
+    warrior.damage = warriorDamage();
+    dragon.healthPoints -= warrior.damage;
+  },
+  playMage: (roundForMage) => {
+    mage.damage = roundForMage().damage;
+    mage.mana = roundForMage().mana;
+    dragon.healthPoints -= mage.damage;
+  },
+  dragonShift: (dragonDamage) => {
+    dragon.damage = dragonDamage();
+    warrior.healthPoints -= dragonDamage();
+    mage.healthPoints -= dragonDamage();
+  },
+  shiftUpdate: () => {
+   return battleMembers;
+  }
+};
+gameActions.warriorShift(warriorDamage);
+gameActions.playMage(roundForMage);
+gameActions.dragonShift(dragonDamage);
+console.table(battleMembers);
 
