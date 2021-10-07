01efa3d0de3eb0f609bcbc4d1c21bcea36dff1b5
Joao-Ortega <joao_ortega17@hotmail.com>
2021-10-07 15:24:50 -0300

    Adicionando eventos por meio de React

--- Front-End/bloco-11-react/dia-03-componentes-e-eventos/fancy-buttons/src/App.js
+++ Front-End/bloco-11-react/dia-03-componentes-e-eventos/fancy-buttons/src/App.js
@@ -4,25 +4,36 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super()
+    this.state = {
+      clicks1: 0,
+      clicks2: 0,
+      clicks3: 0,
+    }
     this.text1 = this.text1.bind(this)
     this.text2 = this.text2.bind(this)
     this.text3 = this.text3.bind(this)
   }
   text1() {
-    console.log('Você clicou no texto 1!')
+    this.setState((stateBefore, _props) => ({
+      clicks1: stateBefore.clicks1 + 1,
+    }))
   }
   text2() {
-    console.log('Você clicou no botão do meio!!')
+    this.setState((stateBefore, _props) => ({
+      clicks2: stateBefore.clicks2 + 1,
+    }))
   }
   text3() {
-    console.log('Você tem apenas 24 horas!!Run')
+    this.setState((stateBefore, _props) => ({
+      clicks3: stateBefore.clicks3 + 1,
+    }))
   }
   render() {
     return (
       <div className="btns-div">
-        <button onClick={this.text1}>Imprime Texto1</button>
-        <button onClick={this.text2}>Imprime Texto2</button>
-        <button onClick={this.text3}>Imprime Texto3</button>
+        <button onClick={this.text1}>{ this.state.clicks1 }</button>
+        <button onClick={this.text2}>{ this.state.clicks2 }</button>
+        <button onClick={this.text3}>{ this.state.clicks3 }</button>
       </div>
     )
   }
