7a9dd98fa1ed321fd8fd4f4e769049c9765cd676
Joao-Ortega <joao_ortega17@hotmail.com>
2021-11-19 13:07:16 -0300

    Exercicios dia 02

---
+++ Front-End/bloco-15-gerenciamento-de-estado-com-redux/dia-02-redux-react/react-redux-app/src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore, compose } from 'redux';
+import rootReducer from '../reducers';
+
+const store = createStore(rootReducer,
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+export default store;
