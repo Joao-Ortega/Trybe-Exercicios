758ca92320685c5c7831355f70f2cfd0c888e045
Joao-Ortega <joao_ortega17@hotmail.com>
2022-03-10 15:07:12 -0300

    Exercises 24.2

---
+++ Back-End/bloco-24-node-ORM-autenticacao/dia-02-associations/association-sequelize/models/Book.js
@@ -0,0 +1,16 @@
+// models/Book.js
+module.exports = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+    bookId: { type: DataTypes.INTEGER, primaryKey: true },
+    name: DataTypes.STRING,
+    releaseYear: DataTypes.INTEGER,
+    numberPages: DataTypes.INTEGER,
+  },
+  {
+    timestamps: false,
+    tableName: 'Books',
+    underscored: true,
+  });
+
+  return Book;
+};
---
+++ Back-End/bloco-24-node-ORM-autenticacao/dia-02-associations/association-sequelize/seeders/20220308182738-user-books.js
@@ -0,0 +1,22 @@
+// cole esse código dentro do arquivo da seed "user-books"
+
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    return queryInterface.bulkInsert('UserBooks',
+      [
+        { user_id: 1, book_id: 1 },
+        { user_id: 1, book_id: 3 },
+        { user_id: 2, book_id: 1 },
+        { user_id: 2, book_id: 2 },
+        { user_id: 3, book_id: 1 },
+        { user_id: 3, book_id: 2 },
+        { user_id: 3, book_id: 3 },
+      ],
+      {},
+    );
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('UserBooks', null, {});
+  },
+};
