804ac9af8e716c7a64885281cd7a7f00a74255fd
Joao-Ortega <joao_ortega17@hotmail.com>
2022-08-10 11:17:04 -0300

    Exercise 03

--- CS/bloco-37-estrutura-de-dados/dia-02-arrays/exercises.py
+++ CS/bloco-37-estrutura-de-dados/dia-02-arrays/exercises.py
@@ -28,12 +28,11 @@ print(shuffle_card_array([2, 6, 4, 5]))
 
 def best_combinations(arr):
     count_best_combinations = 0
-    for el in arr[1:]:
-        for el2 in arr:
-            if arr.index(el) > arr.index(el2):
-                if arr[el] == arr[el2]:
-                    count_best_combinations += 1
+    for el, _nb in enumerate(arr):
+        for el2, _nb2 in enumerate(arr):
+            if el > el2 and arr[el] == arr[el2]:
+                count_best_combinations += 1
     return count_best_combinations
 
 
-print(best_combinations([1, 3, 1, 1, 2, 3]))
+print(best_combinations([1, 1, 2, 3]))
